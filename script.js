a9ecb3c52fffecb56fa2dcaf10717aa78140c5de
Killer-555 <ranjith2503@gmail.com>
2024-05-28 14:47:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let tracks = [];
+let currentTrackIndex = 0;
+const audioElement = document.getElementById('audio');
+const trackTitleElement = document.getElementById('track-title');
+
+async function fetchTracks() {
+    const response = await fetch('https://vibe-music.herokuapp.com/tracks');
+    tracks = await response.json();
+    loadTrack(currentTrackIndex);
+}
+
+
+function loadTrack(index) {
+    if (tracks.length === 0) return;
+    audioElement.src = tracks[index].src;
+    trackTitleElement.innerText = tracks[index].title;
+    audioElement.load();
+    audioElement.play();
+}
+
+function togglePlay() {
+    if (audioElement.paused) {
+        audioElement.play();
+    } else {
+        audioElement.pause();
+    }
+}
+
+function nextTrack() {
+    currentTrackIndex = (currentTrackIndex + 1) % tracks.length;
+    loadTrack(currentTrackIndex);
+}
+
+function prevTrack() {
+    currentTrackIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
+    loadTrack(currentTrackIndex);
+}
+
+// Initialize the player with the first track
+fetchTracks();
